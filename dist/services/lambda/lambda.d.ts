043b360e800618b41896a03416fc58d43a745601
Ilan Torbaty <ilantorbaty@Ilans-MacBook-Pro.local>
2023-09-04 16:23:36 +0300

    fix: folder cleanup

--- dist/services/lambda/lambda.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function getLambdaFunctions(): Promise<import("aws-sdk/lib/request").PromiseResult<import("aws-sdk/clients/lambda").ListFunctionsResponse, import("aws-sdk").AWSError>>;
--- dist/services/lambda/lambda.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getLambdaFunctions = void 0;
-const aws_1 = require("../aws");
-async function getLambdaFunctions() {
-    return await aws_1.lambda.listFunctions().promise();
-}
-exports.getLambdaFunctions = getLambdaFunctions;
-//# sourceMappingURL=lambda.js.map
